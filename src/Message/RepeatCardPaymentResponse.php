bd1cc57238ea96777cd4e65a3a07b1daf0bdfd34
D. Verizhnikov <xdimedrolx@yandex.ru>
2016-01-26 03:35:08 +0300

    Added helper methods for repeatCardPayment response

--- src/Message/RepeatCardPaymentResponse.php
+++ src/Message/RepeatCardPaymentResponse.php
@@ -4,14 +4,25 @@ namespace Omnipay\YandexKassaMws\Message;
 
 class RepeatCardPaymentResponse extends AbstractResponse
 {
+	public function getTransactionId()
+	{
+		return (string) $this->data->attributes()->clientOrderId;
+	}
+
+	public function getInvoiceId()
+	{
+		return isset($this->data->attributes()->invoiceId) ? (string) $this->data->attributes()->invoiceId : null;
+	}
+
 	public function getData()
 	{
 		return array(
-			'clientOrderId' => (int) $this->data->attributes()->clientOrderId,
+			'clientOrderId' => $this->getTransactionId(),
 			'status' => $this->getCode(),
 			'error' => $this->getError(),
 			'processedDT' => new \DateTime((string) $this->data->attributes()->processedDT),
 			'techMessage' => (string) $this->data->attributes()->techMessage,
+			'invoiceId' => $this->getInvoiceId()
 		);
 	}
 }
